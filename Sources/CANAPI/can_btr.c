60e23c0d5cdaa412855e972ee81c1685dbc6199a
Uwe McVogt <mac@uv-software.de>
2024-04-10 14:07:02 +0200

    Fix compiler warnings with legacy CAN 2.0 frame format
    Relates to [CANAPI-83]

--- Sources/CANAPI/can_btr.c
+++ Sources/CANAPI/can_btr.c
@@ -166,6 +166,8 @@ int btr_check_bitrate(const btr_bitrate_t *bitrate, bool fdoe, bool brse) {
         if ((bitrate->btr.nominal.sjw < BTR_NOMINAL_SJW_MIN) || (BTR_NOMINAL_SJW_MAX < bitrate->btr.nominal.sjw))
             return BTRERR_BAUDRATE;
 #if (OPTION_CAN_2_0_ONLY != OPTION_DISABLED)
+        (void)fdoe; (void)brse;  // To avoid compiler warnings
+        
         if ((bitrate->btr.nominal.sam != BTR_NOMINAL_SAM_SINGLE) && (BTR_NOMINAL_SAM_TRIPLE != bitrate->btr.nominal.sam))
             return BTRERR_BAUDRATE;
 #else
@@ -278,6 +280,8 @@ int btr_compare_bitrates(const btr_bitrate_t *bitrate1, const btr_bitrate_t *bit
         if (cmp_sp && (data_sp1 != data_sp2))
             return (data_sp1 < data_sp2) ? -4 : +4;
     }
+#else
+    (void)fdoe; (void)brse;  // To avoid compiler warnings
 #endif
     /* if the bit-rates are equal return 1*/
     return 0;
